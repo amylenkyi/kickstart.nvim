31d01ecea3f2ac002b7abbf1b27e59aafa3fcf48
Andrii Mylenkyi <v-andrii.mylenkyi@choreograph.com>
2024-06-25 23:14:09 +0300

    Telescope: added extension for dir and UI selection.

--- init.lua
+++ init.lua
@@ -340,6 +340,8 @@ require('lazy').setup({
         end,
       },
       { 'nvim-telescope/telescope-ui-select.nvim' },
+      { 'princejoogie/dir-telescope.nvim' },
+      { 'nvim-telescope/telescope-ui-select.nvim' },
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
