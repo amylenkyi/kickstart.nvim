85ab4c5b1a1e5fe2012dd0736439a7c26867e026
Andrii Mylenkyi <v-andrii.mylenkyi@choreograph.com>
2025-05-23 00:07:19 +0300

    Disable auto-formatting some file types on save.

--- init.lua
+++ init.lua
@@ -767,7 +767,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true, typescript = true, ts = true }
+        local disable_filetypes = { c = true, cpp = true, typescript = true, ts = true, python = true }
         return {
           timeout_ms = 500,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
