af8948c3effe3672c15aab2add04e78838128e02
Andrii Mylenkyi <v-andrii.mylenkyi@choreograph.com>
2025-05-23 00:02:47 +0300

    Telescope: LiveGrep Args 2nd part. Enable line numbers in Telescope preview.

--- init.lua
+++ init.lua
@@ -430,6 +430,16 @@ require('lazy').setup({
       -- Enable Telescope extensions if they are installed
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
+      pcall(require('telescope').load_extension, 'live-grep-args')
+
+      vim.api.nvim_create_autocmd('User', {
+        desc = 'Enable line numbers in Telescope preview.',
+        pattern = 'TelescopePreviewerLoaded',
+        group = vim.api.nvim_create_augroup('myconfig-telescope-preview-ln', { clear = true }),
+        callback = function()
+          vim.opt_local.number = true
+        end,
+      })
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
@@ -438,7 +448,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sg', ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
